26b7c33585b78cf5107bbfd95b0175e61589f224
Nayoon Lee <nayoon0530@gmail.com>
2023-08-02 23:58:01 +0900

    calculate urls.py, views.py complete

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -6,7 +6,15 @@ from sendEmail.views import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    # 로그인된 사용자만 접근
+    # 조건문 : 사용자의 정보가 세션에 존재하면 메인 화면으로 출력
+    #          만약 사용자 정보가 세션에 없다면 로그인 화면으로 출력
+    if 'user_name' in request.session.keys():
+        return render(request, 'main/index.html') # 사용자의 세션 정보가 들어있는 상태의 메인 화면
+    else:
+        return redirect('main_signin')
+
+    # return render(request, 'main/index.html') # 그냥 아무 정보 없는 메인 화면
 
 def signup(request):
     return render(request, 'main/signup.html')
@@ -43,6 +51,20 @@ def join(request):
 def signin(request):
     return render(request, 'main/signin.html')
 
+def login(request):
+    # 로그인된 사용자만 이용할 수 있도록 구현
+    # 이 때, 현재 사용자가 로그인된 사용자인지 판단하기 위해 FROM(verify함ㅅ 에서 만든 세션)
+    # 세션 처리 진행.
+    loginEmail = request.POST['loginEmail']
+    loginPW = request.POST['loginPW']
+    user = User.objects.get(user_email = loginEmail)
+    if user.user_password == loginPW: # 로그인 성공하면
+        request.session['user_name'] = user.user_name # 사용자가 회원가입 시 입력한 정보
+        request.session['user_email'] = user.user_email # 사용자가 회원가입 시 입력한 정보
+        return redirect('main_index')
+    else: # 로그인 실패하면
+        return redirect('main_loginFail')
+
 def verifyCode(request):
     return render(request, 'main/verifyCode.html')
 
@@ -63,17 +85,34 @@ def verify(request):
         response = redirect('main_index')
         
         # 저장되어 있는 쿠키 삭제
-        response.delete_cookie('code')
+        response.delete_cookie('code') 
         response.delete_cookie('user_id')
         # response.set_cookie('user', user)
-        request.session['user_name'] = user.user_name
-        request.session['user_email'] = user.user_email
+
+        '''
+        response와 request의 차이
+        response는 main_index에 응답을 주는 것, 즉 index 페이지로 가시오.
+        request는 로그인 화면 구현하는데에 요청을 줌.
+        '''
+
+        # 사용자 정보를 세션에 저장
+        request.session['user_name'] = user.user_name # 로그인 화면 구현
+        request.session['user_email'] = user.user_email # 로그인 화면 구현
         return response
     else:
-        redirect('main_verifyCode')
+        return redirect('main_verifyCode')
 
 
     # return redirect('main_index') # 인증이 완료되면 메인 화면을 보내달라는 의미
 
 def result(request):
-    return render(request, 'main/result.html')
+    if 'user_name' in request.session.keys():
+        return render(request, 'main/result.html') # 사용자의 세션 정보가 들어있는 상태의 메인 화면, 엑셜 결과 페이지까지 세션 유지
+    else:
+        return redirect('main_signin')
+
+def logout(request):
+    # 로그아웃의 개념 : 세션 종료, 즉 세션을 삭제
+    del request.session['user_name']
+    del request.session['user_email']
+    return redirect('main_signin')
